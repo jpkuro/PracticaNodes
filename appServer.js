96a65f9b58ff713d52044461ef0b1cb037292e15
jpkuro <joseluispalmapiguave@gmail.com>
2026-08-16 00:38:32 -0500

    servidor dinamico rutero

--- appServer.js
+++ appServer.js
@@ -1,57 +1,43 @@
-/*let http = require('http');
-let fs = require('fs');
-
-http.createServer(function(req, res){
-
-    console.log('Petición recibida:', req.url);
-
-    if(req.url === '/'){
-
-        fs.readFile('./Paginas/index.html', function(err, data){
-
-            if(err){
-                console.log('Error:', err);
-                res.writeHead(500, {'content-type':'text/plain'});
-                res.end('Error al cargar index.html');
-                return;
-            }
-
-            res.writeHead(200, {'content-type':'text/html'});
-            res.end(data);
-        });
-
-    } else if(req.url === '/generic'){
-
-        fs.readFile('./Paginas/generic.html', function(err, data){
-
-            if(err){
-                console.log('Error:', err);
-                res.writeHead(500, {'content-type':'text/plain'});
-                res.end('Error al cargar generic.html');
-                return;
-            }
-
-            res.writeHead(200, {'content-type':'text/html'});
-            res.end(data);
-        });
-
-    } else {
-
-        res.writeHead(404, {'content-type':'text/plain'});
-        res.end('Página no encontrada');
-    }
-
-}).listen(3000, function(){
-    console.log('Servidor funcionando en http://localhost:3000');
-});*/
 
 let http = require('http');
 let fs = require('fs');
+let path = require('path');
 
 http.createServer(function (req,res){
 
     console.log('Petición recibida:', req.url);
+    let ruta;
+    
+    if(req.url === '/'){
+        ruta = './Paginas/index.html';
+    }else {
+        ruta = '.' + req.url;
+    }
 
+    let extensiones = path.extname(ruta);
+    console.log('la ruta es: ', ruta);
+    console.log('la extencion es: ', extensiones);
+
+    let tipoExt = 'text/plain'
+    if(extensiones === '.html'){
+        tipoExt = 'text/html';
+    }else if(extensiones === '.css'){
+        tipoExt = 'text/css';
+    }else if(extensiones === '.js'){
+        tipoExt = 'text/javascript';
+    }
+
+    fs.readFile(ruta, function(err, data){
+        if(err){
+            res.writeHead(404 , {'content-type': 'text/plain'});
+            res.end('Pagina no encontrada');
+            return;
+        }
+        res.writeHead(200, {'content-type': tipoExt});
+        res.end(data);
+    });
+
+/*
     if(req.url === '/'){
         fs.readFile('./Paginas/index.html', function(err, data){
             res.writeHead(200, {'content-type':'text/html'});
@@ -63,8 +49,8 @@ http.createServer(function (req,res){
             res.writeHead(200, {'content-type':'text/html'});
             res.end(data);
         });
-    }else if(req.url ==='/assets/css/css.cs'){
-        fs.readFile('/assets/css/css.cs', function(err, data){
+    }else if(req.url ==='/assets/css/css.css'){
+        fs.readFile('./assets/css/css.css', function(err, data){
             res.writeHead(200,{'content-type':'text/css'});
             res.end(data);
         })
@@ -77,5 +63,7 @@ http.createServer(function (req,res){
         res.writeHead(404,{"content-type":"text/plain"});
         res.end('Pagina no encontrada');
     }
-
-}).listen(3000);
+    */
+}).listen(3000, () =>{
+    console.log("conexion extablecida");
+});
